de025021f78fbfc97ba8c03b4481c8dd48b7c4b0
Moises Henrique <moihgm67@gmail.com>
2026-06-08 21:12:29 -0300

    fix: update error message in ProductNotFoundException to English

--- src/main/java/com/moisas/barbershop/modules/shared/exceptions/ProductNotFoundException.java
+++ src/main/java/com/moisas/barbershop/modules/shared/exceptions/ProductNotFoundException.java
@@ -2,7 +2,7 @@ package com.moisas.barbershop.modules.shared.exceptions;
 
 public class ProductNotFoundException extends RuntimeException {
     public ProductNotFoundException() {
-        super("Produto não encontrado.");
+        super("Product not found.");
     }
 
     @Override
